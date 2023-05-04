e578dbe9e7eff8aca3a089f5a1f4095547c10b36
guiltychong <132539074+guiltychong@users.noreply.github.com>
2023-05-04 22:33:10 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "wordpress-database-8.cgwcexl2xsrz.us-east-1.rds.amazonaws.com"
-customuser = "root"
-custompass = "aacs3253"
-customdb = "tanxynchongztDatabase"
-
-custombucket = "tanxychongzt-customer"
-customregion = "us-east-1"
